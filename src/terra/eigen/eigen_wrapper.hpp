5283e966a2d0f1dcbd0047497cbd174d0ef0dece
Nils Kohl <nils.kohl@lmu.de>
2025-07-28 16:17:31 +0200

    Adding an Eigen wrapper and CMake integration.

---
+++ src/terra/eigen/eigen_wrapper.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-but-set-variable"
+
+#include "../../../extern/eigen-3.4.0/Eigen/Eigen"
+
+#pragma clang diagnostic pop
