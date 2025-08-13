5665e6246bd98f57d24cc434460399440873a79e
Nils Kohl <nils.kohl@lmu.de>
2025-08-13 09:37:50 +0200

    Linalg namespace documentation.

---
+++ src/terra/linalg/linalg.hpp
@@ -0,0 +1,15 @@
+
+#pragma once
+
+/// @namespace terra::linalg
+/// @brief Contains linear algebra utilities and functions for the Terra project.
+///
+/// This namespace provides vector and matrix operations, including linear combinations,
+/// dot products, and vector manipulations. It defines concepts for vector-like types
+/// and block 2-vectors, ensuring that types conform to expected interfaces for linear algebra
+/// operations. The utilities are designed to work with various vector implementations, allowing
+/// for flexibility and extensibility in numerical computations.
+///
+/// @note For both, vector-vector and operator-vector operations, users should use the free functions provided in
+/// vector.hpp and operator.hpp.
+namespace terra::linalg {}
