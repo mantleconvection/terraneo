bb8bbc99a630a2f81bb17b0c38e8cc4cd90d2928
Nils Kohl <nils.kohl@lmu.de>
2025-10-07 11:41:21 +0200

    More verbose run info printer.

--- src/terra/util/info.hpp
+++ src/terra/util/info.hpp
@@ -1,24 +1,80 @@
 
 #pragma once
+#include <filesystem>
+
 #include "kokkos/kokkos_wrapper.hpp"
 #include "table.hpp"
 
 namespace terra::util {
 
-inline Table info_table()
+/// @brief Prints some general information for this run to out (set, e.g., to std::cout) on the root process.
+inline void print_general_info_on_root( int argc, char** argv, std::ostream& out )
 {
-    Table table;
+    if ( mpi::rank() != 0 )
+    {
+        return;
+    }
 
-    const auto threads = Kokkos::num_threads();
-    table.add_row( { { "key", "threads" }, { "value", threads } } );
+    using clock      = std::chrono::system_clock;
+    const auto now   = clock::now();
+    const auto now_c = clock::to_time_t( now );
 
-    const auto devices = Kokkos::num_devices();
-    table.add_row( { { "key", "devices" }, { "value", devices } } );
+    out << "=========================================\n";
+    out << "          TerraNeo X - Run Info          \n";
+    out << "=========================================\n";
 
+    out << "Wall time start : " << std::put_time( std::localtime( &now_c ), "%Y-%m-%d %H:%M:%S" ) << "\n";
+
+    // Binary path and working directory
+    try
+    {
+        std::filesystem::path bin_path( argv[0] );
+        bin_path = std::filesystem::absolute( bin_path );
+        out << "Executable path : " << bin_path << "\n";
+        out << "Executable dir  : " << bin_path.parent_path() << "\n";
+        out << "Working dir     : " << std::filesystem::current_path() << "\n";
+    }
+    catch ( ... )
+    {
+        out << "Executable path : (unavailable)\n";
+    }
+
+    // Command-line arguments
+    out << "Command line    :";
+    for ( int i = 0; i < argc; ++i )
+    {
+        out << " " << argv[i];
+    }
+    out << "\n";
+
+    // Parallel resources
+    const auto threads   = Kokkos::num_threads();
+    const auto devices   = Kokkos::num_devices();
     const auto mpi_procs = mpi::num_processes();
-    table.add_row( { { "key", "MPI processes" }, { "value", mpi_procs } } );
+    const auto mpi_rank  = mpi::rank();
 
-    return table;
+    out << "MPI processes   : " << mpi_procs << "\n";
+    out << "MPI rank        : " << mpi_rank << "\n";
+    out << "Kokkos threads  : " << threads << "\n";
+    out << "Kokkos devices  : " << devices << "\n";
+
+    // Kokkos defaults
+    using ExecSpace = Kokkos::DefaultExecutionSpace;
+    using MemSpace  = ExecSpace::memory_space;
+    out << "ExecSpace       : " << ExecSpace::name() << "\n";
+    out << "MemSpace        : " << MemSpace::name() << "\n";
+
+    // Optional: print host/system info if available
+#if defined( __linux__ )
+    char hostname[256];
+    if ( gethostname( hostname, sizeof( hostname ) ) == 0 )
+    {
+        out << "Hostname        : " << hostname << "\n";
+    }
+#endif
+
+    // Separator footer
+    out << "=========================================\n" << std::endl;
 }
 
 } // namespace terra::util
